79ca97632b0f8049d6a0d69c43778a2d6f95073f
yashwant gosavi <yashwantgosavi0@gmail.com>
2024-01-16 13:23:29 +0530

    update

--- Learning/11DOM/II-events.js
+++ Learning/11DOM/II-events.js
@@ -4,3 +4,27 @@ let btn = document.getElementById('hello');
 btn.addEventListener('click', function(){
     alert('hello');
 })
+
+// onclick
+// mouseover
+// mouseout
+// keypress
+// keydown
+// scroll
+// mouseenter
+// mousedown
+// mouseleave
+// mouseup
+
+// classList.add
+// classList.remove
+// classList.toggle
+
+        // event.keyCode
+
+
+// ** Propagation of events **
+        // event.stopPropagation
+
+// Strict Mode
+
