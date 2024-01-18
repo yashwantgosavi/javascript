bd6cb6f18cd8e4475df26ab2f65185820ae779b0
yashwant gosavi <yashwantgosavi0@gmail.com>
2024-01-18 16:01:35 +0530

    update

--- projects/1calculator/style.js
+++ projects/1calculator/style.js
@@ -10,25 +10,20 @@ var switchBtn = document.getElementById("switchBtn");
 var section = document.getElementsByTagName("section");
 var outputScreen = document.getElementById("outputScreen");
 
-// darkLight toggle -----------------------------------
-switchBtn.addEventListener("click", function () {
-  switchBtn.classList.toggle("rightPosition");
-  section[0].classList.toggle("lightMode");
-});
 
 // calculator ------------------------------------------
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("click", function () {
     var value = this.getAttribute("data-value");
     console.log(value);
-    
+
     if (value == "backSpace") {
       // Handle the backspace functionality
       let currentText = outputScreen.textContent;
       outputScreen.textContent = currentText.slice(0, -1);
       return;
     }
-    
+
     outputScreen.style.color = "white";
 
     if (
